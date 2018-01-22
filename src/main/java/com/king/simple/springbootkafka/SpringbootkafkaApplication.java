f806dccdd2f5e1149bc4bac363dbf3fcb6257655
kingflag <1362168862@qq.com>
2018-01-22 15:13:02 +0800

    first commit 初始化springboot

---
+++ src/main/java/com/king/simple/springbootkafka/SpringbootkafkaApplication.java
@@ -0,0 +1,12 @@
+package com.king.simple.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootkafkaApplication.class, args);
+	}
+}
